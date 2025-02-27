322bd159122f191d03cbe97217ef22aaec3429c1
Patrick <patrick.korhonen2@gmail.com>
2025-02-27 21:26:05 +0200

    refactor TrendingShops component for improved layout and styling

--- omakoju-front/app/components/shopAccordion.tsx
+++ omakoju-front/app/components/shopAccordion.tsx
@@ -44,10 +44,7 @@ const TrendingShops = () => {
       defaultValue={["trending", "new", "favorites"]}
       type="multiple"
     >
-      <AccordionItem
-        value="trending"
-        className=" bg-gray-50 p-4 rounded-xl border-0"
-      >
+      <AccordionItem value="trending" className="  p-4 rounded-xl border-0">
         <AccordionTrigger className="text-xl uppercase font-bold rounded hover:no-underline">
           Trending
         </AccordionTrigger>
@@ -57,55 +54,61 @@ const TrendingShops = () => {
             <CarouselNext className="text-2xl font-bold h-full rounded"></CarouselNext>
             <CarouselContent>
               {shops.slice(0, 8).map((shop, index) => (
-                <CarouselItem className="basis-1/3 pl-4 pr-4" key={index}>
-                  <Link href={`/shop/${shop.id}`} key={index}>
-                    <div className="relative">
-                      {shop.bannerPicture ? (
-                        <Image
-                          src={shop.bannerPicture}
-                          alt={shop.shopName}
-                          width={0}
-                          height={0}
-                          style={{ width: "100%", height: "auto" }}
-                          className="rounded"
-                        />
-                      ) : (
-                        <Image
-                          src={"/photos/default_banner.png"}
-                          alt={shop.shopName}
-                          width={0}
-                          height={0}
-                          style={{ width: "100%", height: "auto" }}
-                          className="rounded"
-                        />
-                      )}
-                    </div>
-                    <div className="-translate-y-1/4">
-                      {shop.logoPicture && (
-                        <div className="flex justify-center">
+                <CarouselItem className="basis-1/3 pl-5 pr-5 flex" key={index}>
+                  <div className="rounded-xl shadow-lg mb-4">
+                    <Link href={`/shop/${shop.id}`} key={index}>
+                      <div className="relative ">
+                        {shop.bannerPicture ? (
                           <Image
-                            src={shop.logoPicture}
+                            src={shop.bannerPicture}
                             alt={shop.shopName}
                             width={0}
                             height={0}
-                            style={{ width: "30%", height: "auto" }}
-                            className=" rounded-full"
+                            style={{ width: "100%", height: "auto" }}
+                            className="rounded-t-xl shadow-md"
                           />
-                        </div>
-                      )}
-                    </div>
-                    <div className="-translate-y-1/4">
-                      <h2 className="text-xl font-bold">{shop.shopName}</h2>
-                      <p className="text-sm line-clamp-2">{shop.description}</p>
-                    </div>
-                  </Link>
+                        ) : (
+                          <Image
+                            src={"/photos/default_banner.png"}
+                            alt={shop.shopName}
+                            width={0}
+                            height={0}
+                            style={{ width: "100%", height: "auto" }}
+                            className="rounded-t-xl shadow-md"
+                          />
+                        )}
+                      </div>
+                      <div className="-translate-y-3.5">
+                        {shop.logoPicture && (
+                          <div className="flex justify-center">
+                            <Image
+                              src={shop.logoPicture}
+                              alt={shop.shopName}
+                              width={0}
+                              height={0}
+                              style={{ width: "40%", height: "auto" }}
+                              className="rounded-full shadow-md"
+                            />
+                          </div>
+                        )}
+                      </div>
+                      <div className="mx-4 pb-4">
+                        <h2 className="text-xl font-bold">{shop.shopName}</h2>
+                        <p className="text-sm line-clamp-3">
+                          {shop.description}
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
+
             <CarouselPrevious className="text-2xl font-bold bg-slate-50 h-1/3 rounded" />
             <CarouselNext className="text-2xl font-bold bg-slate-50 h-1/3 rounded" />
           </Carousel>
         </AccordionContent>
+        <hr className="border"></hr>
       </AccordionItem>
     </Accordion>
   );
