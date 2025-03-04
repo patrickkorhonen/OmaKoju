5b0916d6a8aa88803458ebad0f50dc89d79e0d78
Patrick <patrick.korhonen2@gmail.com>
2025-03-04 15:45:52 +0200

    enhance product card component with improved styling and image display

--- omakoju-front/app/(main)/shop/components/productCard.tsx
+++ omakoju-front/app/(main)/shop/components/productCard.tsx
@@ -1,24 +1,36 @@
-"use client"
+"use client";
 import Image from "next/image";
 
 type Product = {
-    product: product
-}
+  product: product;
+};
 
 interface product {
-    id: string;
-    shopId: string;
-    name: string;
-    price: number;
-    stock: number;
-    imageUrl: string[];
+  id: string;
+  shopId: string;
+  name: string;
+  price: number;
+  stock: number;
+  imageUrl: string[];
 }
 
-export default function ProductCard({
-    product,
-}: Product) {
-
-    return (
-        <div className="bg-gray-200 p-4"></div>
-    )
+export default function ProductCard({ product }: Product) {
+  return (
+    <div className="shadow-lg shadow-gray-300 rounded-xl">
+      <div className="relative w-full h-64">
+        <Image
+          src={product.imageUrl[0]}
+          alt={"tuote"}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-xl"
+        />
+      </div>
+      <div className="flex flex-col gap-2 p-4">
+        <p className="font-semibold">{product.name}</p>
+        <p className="text-gray-600 text-sm">Lyhyt kuvaus tuotteesta.</p>
+        <p className="font-bold text-end my-2 text-xl">{product.price} €</p>
+      </div>
+    </div>
+  );
 }
