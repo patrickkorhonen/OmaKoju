476c8f53caa5e0116a2bafa9e7a3b0fd6420722e
Patrick <patrick.korhonen2@gmail.com>
2025-03-19 13:05:48 +0200

    Add deleteProduct route

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { addProduct } from "../controllers/productControllers.js";
+import { addProduct, deleteProduct } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.post("/add", authMiddleware, addProduct)
+
+router.delete("/delete", authMiddleware, deleteProduct)
 
 export default router;
