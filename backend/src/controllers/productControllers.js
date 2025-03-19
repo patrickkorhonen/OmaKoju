7fb787821f85cfa5a651f5c684a0cc37fecc9110
Patrick <patrick.korhonen2@gmail.com>
2025-03-19 13:05:36 +0200

    Add deleteProduct function

--- backend/src/controllers/productControllers.js
+++ backend/src/controllers/productControllers.js
@@ -1,38 +1,64 @@
 import prisma from "../prismaClient.js";
 
 export const addProduct = async (req, res) => {
-    const { shopId, name, price, stock, imageUrl } = req.body;
-    const UID = req.userId;
+  const { shopId, name, price, stock, imageUrl } = req.body;
+  const UID = req.userId;
 
-    try {
-        const ownShop = await prisma.shop.findUnique({
-            where: {
-              id: shopId,
-              userId: UID
-            },
-          });
-      
-          if (!ownShop) {
-            return res
-              .status(409)
-              .send({ message: "Unauthorized action" });
-          }
+  try {
+    const ownShop = await prisma.shop.findUnique({
+      where: {
+        id: shopId,
+        userId: UID,
+      },
+    });
 
-        const product = await prisma.product.create({
-            data: {
-                shopId,
-                name,
-                price,
-                stock,
-                imageUrl,
-            }
-        })
-
-        res.json({
-            product
-        })
-    } catch (err) {
-        console.log("error", err)
-        res.status(503).json({ message: "Adding product failed" });
+    if (!ownShop) {
+      return res.status(409).send({ message: "Unauthorized action" });
     }
-}
+
+    const product = await prisma.product.create({
+      data: {
+        shopId,
+        name,
+        price,
+        stock,
+        imageUrl,
+      },
+    });
+
+    res.json({
+      product,
+    });
+  } catch (err) {
+    console.log("error", err);
+    res.status(503).json({ message: "Adding product failed" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { shopId, id } = req.body;
+  const UID = req.userId;
+  try {
+    const ownShop = await prisma.shop.findUnique({
+      where: {
+        id: shopId,
+        userId: UID,
+      },
+    });
+
+    if (!ownShop) {
+      return res.status(409).send({ message: "Unauthorized action" });
+    }
+
+    const product = await prisma.product.delete({
+      where: {
+        id,
+        shopId,
+      },
+    });
+    res.status(204).json({ message: "Product deleted." });
+  } catch (err) {
+    console.log(err);
+    res.status(503).json({ message: "Error while deleting the product." });
+  }
+};
