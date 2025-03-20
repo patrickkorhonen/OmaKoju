c3aea762fef65182feaa77aa94baac92d89babc0
Patrick <patrick.korhonen2@gmail.com>
2025-03-20 23:39:19 +0200

    Add toast notification system with Toaster component and useToast hook

--- omakoju-front/app/(main)/layout.tsx
+++ omakoju-front/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { UserProvider } from "../context/context";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,6 +30,7 @@ export default function RootLayout({
             <Navbar />
           </div>
           {children}
+          <Toaster />
           <Footer />
         </body>
       </UserProvider>
