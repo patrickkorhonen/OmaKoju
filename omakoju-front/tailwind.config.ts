3d1cba798bfcbcadf44694a74b5f80dd3cce09d7
Patrick <patrick.korhonen2@gmail.com>
2025-03-20 23:39:48 +0200

    Update Tailwind CSS configuration to add bgGreenHover color

--- omakoju-front/tailwind.config.ts
+++ omakoju-front/tailwind.config.ts
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-	  './pages/**/*.{js,ts,jsx,tsx}',
-	  './components/**/*.{js,ts,jsx,tsx}',
-	  './app/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-	  extend: {
-		fontFamily: {
-		  sans: ['var(--font-inter)'],
-		  mono: ['var(--font-roboto-mono)'],
-		},
-		colors: {
-			bgGreen: '#224437',
-			bgBeige: '#E8DAC0',
-		}
-	  },
-	},
-	plugins: [],
-  }
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+      colors: {
+        bgGreen: "#224437",
+        bgGreenHover: "#1a362b",
+        bgBeige: "#E8DAC0",
+      },
+    },
+  },
+  plugins: [],
+};
