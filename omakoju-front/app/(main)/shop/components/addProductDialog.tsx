899c678f2af9ace3168e713c0274862d298a31dc
Patrick <patrick.korhonen2@gmail.com>
2025-03-21 11:41:07 +0200

    include loading state and improve product addition logic

--- omakoju-front/app/(main)/shop/components/addProductDialog.tsx
+++ omakoju-front/app/(main)/shop/components/addProductDialog.tsx
@@ -10,15 +10,45 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { useToast } from "@/hooks/use-toast";
+import { CreateProduct } from "@/app/api/product";
+import MoonLoader from "react-spinners/MoonLoader";
 
 import { useState } from "react";
 
-export default function AddProductDialog() {
+export default function AddProductDialog({ id }: { id: string }) {
   const { toast } = useToast();
-  const [name, setName] = useState<string>();
+  const [name, setName] = useState<string>("");
   const [price, setPrice] = useState<string>("");
-  const [stock, setStock] = useState<number>();
+  const [stock, setStock] = useState<string | null>();
   //const [imageUrl, setImageUrl] = useState<null | string>(null);
+  const [uploading, setUploading] = useState(false);
+
+  const addProduct = async () => {
+    if (id && name && price && stock) {
+      const response = await CreateProduct(
+        Number(id),
+        name,
+        Number(price),
+        Number(stock),
+        null
+      );
+      if (response && response.ok) {
+        setUploading(false);
+        toast({
+          className:
+            "bg-green-500 rounded-none border-0 text-white font-bold p-8",
+          description: "Product added succesfully",
+        });
+      } else {
+        setUploading(false);
+        toast({
+          className:
+            "bg-red-500 rounded-none border-0 text-white font-bold p-8",
+          description: "Adding product failed",
+        });
+      }
+    }
+  };
 
   return (
     <Dialog>
@@ -71,29 +101,57 @@ export default function AddProductDialog() {
               Product Stock*
             </label>
             <input
-              type="text"
+              type="number"
+              min="0"
+              step="1"
               name="productStock"
               id="productStock"
               className="mb-2 p-2 w-full border border-gray-300 focus:border-black outline-none rounded-md"
-              value={stock}
-              onChange={(e) => setStock(Number(e.target.value))}
+              value={stock ? stock : ""}
+              onChange={(e) => setStock(e.target.value)}
             />
           </div>
         </DialogHeader>
         <DialogFooter>
           <DialogClose asChild>
-            <button
+            {/* <button
               onClick={() => {
-                toast({
-                  className:
-                    "bg-green-500 rounded-none border-0 text-white font-bold p-8",
-                  description: "Product added succesfully",
-                });
+                setUploading(true)
+                addProduct()
               }}
               className="bg-bgGreen hover:bg-bgGreenHover px-4 py-2 text-white font-semibold"
             >
               Add
-            </button>
+            </button> */}
+            {uploading ? (
+              <div className="bg-bgGreen items-center flex rounded cursor-default font-bold text-white col-span-2 p-2">
+                <MoonLoader
+                  color={"#ffffff"}
+                  cssOverride={{
+                    display: "block",
+                    margin: "0 auto",
+                  }}
+                  size={15}
+                  aria-label="Loading"
+                  data-testid="loader"
+                />
+              </div>
+            ) : (
+              <button
+                disabled={!name || !price || !stock}
+                onClick={() => {
+                  setUploading(true);
+                  addProduct();
+                }}
+                className={
+                  name && price && stock
+                    ? "bg-bgGreen rounded font-bold text-white col-span-2 p-2"
+                    : "bg-[#617f65] rounded cursor-default font-bold text-white col-span-2 p-2"
+                }
+              >
+                Add
+              </button>
+            )}
           </DialogClose>
         </DialogFooter>
       </DialogContent>
