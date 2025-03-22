074407e2655ec49b2ca0b25d06aada3b42f3583a
Patrick <patrick.korhonen2@gmail.com>
2025-03-22 16:30:26 +0200

    fetch product data

--- omakoju-front/app/(main)/product/components/productComponent.tsx
+++ omakoju-front/app/(main)/product/components/productComponent.tsx
@@ -1,7 +1,10 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { GetProduct } from "@/app/api/product";
+import { Product } from "@/interface";
+import { PiSmileySadLight } from "react-icons/pi";
 
 interface ProductComponentProps {
   id: string;
@@ -20,35 +23,38 @@ interface ProductComponentProps {
 //   imageUrl: string[];
 // }
 
-const dummy = {
-  item: {
-    id: "1",
-    shopId: "fc4a64b1-3b99-4e45-b892-49578a3db00f",
-    seller: "Camera Shop",
-    name: "Kamera",
-    price: 109.99,
-    stock: 52,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nunc turpis, pulvinar sed massa ac, tempus sollicitudin eros. Etiam semper maximus erat sed fermentum. Sed in velit eget orci mattis luctus id quis lorem. Ut risus lorem, commodo in porta.",
-    imageUrl: [
-      "/photos/taulu.jpg",
-      "/photos/kamera.jpg",
-      "/photos/majakka.jpg",
-      "/photos/taulu.jpg",
-      "/photos/kamera.jpg",
-      "/photos/majakka.jpg",
-      "/photos/taulu.jpg",
-      "/photos/kamera.jpg",
-      "/photos/majakka.jpg",
-    ],
-  },
-};
+const imageUrl = [
+  "/photos/taulu.jpg",
+  "/photos/kamera.jpg",
+  "/photos/majakka.jpg",
+  "/photos/taulu.jpg",
+  "/photos/kamera.jpg",
+  "/photos/majakka.jpg",
+  "/photos/taulu.jpg",
+  "/photos/kamera.jpg",
+  "/photos/majakka.jpg",
+];
 
 export default function ProductComponent({ id }: ProductComponentProps) {
   const [imageNumber, setImageNumber] = useState<number>(0);
+  const [product, setProduct] = useState<Product>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const response = await GetProduct(id);
+      if (response && response.ok) {
+        const data = await response.json();
+        console.log("tämä on tuote", data);
+        setProduct(data);
+      }
+      setLoading(false);
+    };
+    fetchProduct();
+  }, [id]);
 
   const handleNextImage = () => {
-    if (imageNumber == dummy.item.imageUrl.length - 1) {
+    if (imageNumber == imageUrl.length - 1) {
       setImageNumber(0);
     } else {
       setImageNumber(imageNumber + 1);
@@ -57,80 +63,89 @@ export default function ProductComponent({ id }: ProductComponentProps) {
 
   const handlePreviousImage = () => {
     if (imageNumber == 0) {
-      setImageNumber(dummy.item.imageUrl.length - 1);
+      setImageNumber(imageUrl.length - 1);
     } else {
       setImageNumber(imageNumber - 1);
     }
   };
-
   return (
     <div className="sm:min-h-screen lg:my-8 p-4 xl:p-0 ">
-      <div className="w-full sm:h-[70vh]  xl:w-2/3 grid sm:grid-cols-2 gap-8 p-4 place-self-center mb-40">
-        <div className="sm:h-[60vh] h-60 ">
-          <div className="relative w-full h-full bg-gray-50 rounded-xl">
-            <Image
-              src={dummy.item.imageUrl[imageNumber]}
-              alt={"tuote"}
-              layout="fill"
-              objectFit="contain"
-              className="rounded-xl"
-            />
-            <div className="flex w-full  px-4 top-1/2 justify-between absolute">
-              <button
-                onClick={() => handlePreviousImage()}
-                className="bg-black p-4 rounded-full opacity-60 text-white hover:opacity-80"
-              >
-                <FaArrowLeft />
-              </button>
-              <button
-                onClick={() => handleNextImage()}
-                className="bg-black p-4 rounded-full opacity-60 text-white hover:opacity-80"
-              >
-                <FaArrowRight />
-              </button>
+      {product && (
+        <main>
+          <div className="w-full sm:h-[70vh]  xl:w-2/3 grid sm:grid-cols-2 gap-8 p-4 place-self-center mb-40">
+            <div className="sm:h-[60vh] h-60 ">
+              <div className="relative w-full h-full bg-gray-50 rounded-xl">
+                <Image
+                  src={imageUrl[imageNumber]}
+                  alt={"tuote"}
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-xl"
+                />
+                <div className="flex w-full  px-4 top-1/2 justify-between absolute">
+                  <button
+                    onClick={() => handlePreviousImage()}
+                    className="bg-black p-4 rounded-full opacity-60 text-white hover:opacity-80"
+                  >
+                    <FaArrowLeft />
+                  </button>
+                  <button
+                    onClick={() => handleNextImage()}
+                    className="bg-black p-4 rounded-full opacity-60 text-white hover:opacity-80"
+                  >
+                    <FaArrowRight />
+                  </button>
+                </div>
+              </div>
+              <div className="hidden sm:flex gap-4 overflow-x-auto my-4 ">
+                {imageUrl.map((image, index) => (
+                  <div key={index}>
+                    <button
+                      onClick={() => setImageNumber(index)}
+                      className="relative h-20 w-20"
+                    >
+                      <Image
+                        src={image}
+                        alt={"tuote"}
+                        layout="fill"
+                        objectFit="contain"
+                        className="rounded-xl border h-20 w-full"
+                      />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="grid grid-cols-3 ">
+              <div className="sm:col-span-3 2xl:col-span-2">
+                <p className="text-sm">{product.shop.shopName}</p>
+                <h1 className="text-4xl">{product.name}</h1>
+                <div className="flex flex-col gap-8 my-8">
+                  <p className="text-xl tracking-wide">{product.price} €</p>
+                  <p className="">Stock: {product.stock}</p>
+                  <div className="flex flex-col gap-2">
+                    <button className="w-full border border-blue-500 hover:shadow-sm hover:shadow-blue-600  text-blue-500 font-bold rounded p-2">
+                      Add to Cart
+                    </button>
+                    <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded p-2">
+                      Buy Now
+                    </button>
+                  </div>
+                  <p className="tracking-wide">tuotteen kuvaus</p>
+                </div>
+              </div>
+              <></>
             </div>
           </div>
-          <div className="hidden sm:flex gap-4 overflow-x-auto my-4 ">
-            {dummy.item.imageUrl.map((image, index) => (
-              <div key={index}>
-                <button
-                  onClick={() => setImageNumber(index)}
-                  className="relative h-20 w-20"
-                >
-                  <Image
-                    src={image}
-                    alt={"tuote"}
-                    layout="fill"
-                    objectFit="contain"
-                    className="rounded-xl border h-20 w-full"
-                  />
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="grid grid-cols-3 ">
-          <div className="sm:col-span-3 2xl:col-span-2">
-            <p className="text-sm">{dummy.item.seller}</p>
-            <h1 className="text-4xl">{dummy.item.name}</h1>
-            <div className="flex flex-col gap-8 my-8">
-              <p className="text-xl tracking-wide">{dummy.item.price} €</p>
-              <p className="">Stock: Last Piece</p>
-              <div className="flex flex-col gap-2">
-              <button className="w-full border border-blue-500 hover:shadow-sm hover:shadow-blue-600  text-blue-500 font-bold rounded p-2">
-                Add to Cart
-              </button>
-              <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded p-2">
-                Buy Now
-              </button>
-              </div>
-              <p className="tracking-wide">{dummy.item.description}</p>
-            </div>
-          </div>
-          <></>
-        </div>
-      </div>
-      <hr className="mx-10"></hr>
+          <hr className="mx-10"></hr>
+        </main>
+      )}
+      {!product && !loading && (
+        <main className="min-h-screen flex gap-2 justify-center items-center -mt-16">
+          <p className="text-2xl">Product Not Found</p>
+          <PiSmileySadLight className="text-2xl" />
+        </main>
+      )}
     </div>
   );
 }
