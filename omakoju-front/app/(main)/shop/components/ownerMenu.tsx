239b0c8aa29a9e72beba11bcaa723657478de7fe
Patrick <patrick.korhonen2@gmail.com>
2025-03-24 23:02:25 +0200

    add informational message for shop owner visibility

--- omakoju-front/app/(main)/shop/components/ownerMenu.tsx
+++ omakoju-front/app/(main)/shop/components/ownerMenu.tsx
@@ -12,6 +12,7 @@ type addProduct = {
 export default function OwnerMenu({ id, products, setProducts }: addProduct) {
   return (
     <div className="hidden xl:block w-[15%] h-full absolute border-r p-4 py-8">
+      <p className="text-xs text-slate-700 mb-4">This menu shows only for the shop owner and customers won&apos;t see it.</p>
       <AddProductDialog id={id} products={products} setProducts={setProducts}/>
     </div>
   );
