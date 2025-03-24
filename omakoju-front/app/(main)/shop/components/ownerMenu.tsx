dc629fd403fcb852ac70d1a4b89276862c48cabb
Patrick <patrick.korhonen2@gmail.com>
2025-03-24 22:44:46 +0200

    products and setProducts props for improved state management

--- omakoju-front/app/(main)/shop/components/ownerMenu.tsx
+++ omakoju-front/app/(main)/shop/components/ownerMenu.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { Dispatch, SetStateAction } from "react";
 import AddProductDialog from "./addProductDialog";
+import { Product } from "@/interface";
 
-export default function OwnerMenu({ id }: { id: string }) {
+type addProduct = {
+  id: string;
+  products: Product[]
+  setProducts: Dispatch<SetStateAction<Product[]>>;
+}
+
+export default function OwnerMenu({ id, products, setProducts }: addProduct) {
   return (
     <div className="hidden xl:block w-[15%] h-full absolute border-r p-4 py-8">
-      <AddProductDialog id={id}/>
+      <AddProductDialog id={id} products={products} setProducts={setProducts}/>
     </div>
   );
 }
