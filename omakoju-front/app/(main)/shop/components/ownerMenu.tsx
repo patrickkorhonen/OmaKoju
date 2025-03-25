bd5a5f1c177a6ef7d7d9872fb767feadecb4abac
Patrick <patrick.korhonen2@gmail.com>
2025-03-25 23:25:27 +0200

    enhanced product management

--- omakoju-front/app/(main)/shop/components/ownerMenu.tsx
+++ omakoju-front/app/(main)/shop/components/ownerMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Dispatch, SetStateAction } from "react";
 import AddProductDialog from "./addProductDialog";
+import ManageProducts from "./manageProducts";
 import { Product } from "@/interface";
 
 type addProduct = {
@@ -11,9 +12,10 @@ type addProduct = {
 
 export default function OwnerMenu({ id, products, setProducts }: addProduct) {
   return (
-    <div className="hidden xl:block w-[15%] h-full absolute border-r p-4 py-8">
+    <div className="hidden xl:flex flex-col w-[15%] gap-4 h-full absolute border-r p-4 py-8 overflow-y-auto">
       <p className="text-xs text-slate-700 mb-4">This menu shows only for the shop owner and customers won&apos;t see it.</p>
       <AddProductDialog id={id} products={products} setProducts={setProducts}/>
+      <ManageProducts products={products}/>
     </div>
   );
 }
