e06989d0ee693e05c7d2095036c627c447a621c6
Patrick <patrick.korhonen2@gmail.com>
2025-03-26 15:55:38 +0200

    update homepage section with gradient background and enhanced welcome message

--- omakoju-front/app/(main)/page.tsx
+++ omakoju-front/app/(main)/page.tsx
@@ -5,10 +5,10 @@ import TrendingShops from "../components/shopAccordion";
 export default function Home() {
   return (
     <main className="min-h-screen ">
-      <section className="flex flex-col bg-bgGreen h-80 w-full mb-8">
-        <div className="flex flex-col gap-4 h-full cursor-default justify-center items-center">
-          <h1 className="text-3xl sm:text-5xl font-bold text-white">Welcome to Omakoju</h1>
-          <div className="text-white text-sm sm:text-base flex font-semibold">The place to find the best&nbsp;<p className="text-bgBeige font-bold">shops</p>&nbsp;and&nbsp;<p className="text-bgBeige font-bold">customers</p></div>
+      <section className="flex flex-col bg-gradient-to-br from-bgGreen to-[#44806a] h-96 w-full mb-8">
+        <div className="flex flex-col gap-8 h-full cursor-default justify-center items-center">
+          <h1 className="text-3xl sm:text-6xl font-bold text-white">Welcome to Omakoju.</h1>
+          <p className="text-white text-sm flex sm:w-1/4 text-center font-semibold">Unleash your creativity and start selling with ease. Join our vibrant community of entrepreneurs and explore countless shops tailored just for you.</p>
         </div>
       </section>
       <div className="w-screen sm:w-2/3 flex flex-col place-self-center">
