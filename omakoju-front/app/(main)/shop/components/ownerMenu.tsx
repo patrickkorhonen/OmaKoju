17d78400c0bdb1865c2f67527d67aec11bc64898
Patrick <patrick.korhonen2@gmail.com>
2025-03-26 15:56:03 +0200

    add setProducts prop to ManageProducts component in OwnerMenu

--- omakoju-front/app/(main)/shop/components/ownerMenu.tsx
+++ omakoju-front/app/(main)/shop/components/ownerMenu.tsx
@@ -15,7 +15,7 @@ export default function OwnerMenu({ id, products, setProducts }: addProduct) {
     <div className="hidden xl:flex flex-col w-[15%] gap-4 h-full absolute border-r p-4 py-8 overflow-y-auto">
       <p className="text-xs text-slate-700 mb-4">This menu shows only for the shop owner and customers won&apos;t see it.</p>
       <AddProductDialog id={id} products={products} setProducts={setProducts}/>
-      <ManageProducts products={products}/>
+      <ManageProducts products={products} setProducts={setProducts}/>
     </div>
   );
 }
