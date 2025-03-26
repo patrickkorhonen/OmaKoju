90a0ac6eea54ef9a6732c5c2e3a919d9a96848fa
Patrick <patrick.korhonen2@gmail.com>
2025-03-26 22:08:42 +0200

    use shopId prop in AddProductDialog and ManageProducts components

--- omakoju-front/app/(main)/shop/components/ownerMenu.tsx
+++ omakoju-front/app/(main)/shop/components/ownerMenu.tsx
@@ -14,8 +14,8 @@ export default function OwnerMenu({ id, products, setProducts }: addProduct) {
   return (
     <div className="hidden xl:flex flex-col w-[15%] gap-4 h-full absolute border-r p-4 py-8 overflow-y-auto">
       <p className="text-xs text-slate-700 mb-4">This menu shows only for the shop owner and customers won&apos;t see it.</p>
-      <AddProductDialog id={id} products={products} setProducts={setProducts}/>
-      <ManageProducts products={products} setProducts={setProducts}/>
+      <AddProductDialog shopId={id} products={products} setProducts={setProducts}/>
+      <ManageProducts shopId={id} products={products} setProducts={setProducts}/>
     </div>
   );
 }
